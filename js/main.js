d0e6779ee54e96446f1011be75136b03fec6d4f6
Bianca Togonon <biancatogodev@gmail.com>
2022-04-10 19:06:20 -0700

    clean up js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,25 @@
-// // https://github.com/AugustoMarcelo/mcuapi
-
 document.querySelector('button').addEventListener('click', getMCU)
 
 function getMCU() {
 
     let id = Number(document.querySelector('input').value)
-    console.log(id)
-    console.log(`${id} after assignment`)
+
+    // carousel // need to rework
     document.querySelector('.leftArrow').addEventListener('click', function() {
         id -= 1
-        console.log(`${id} Left arrow`)
         goFetch(id)
-        console.log(`${id} Left arrow after call`)
     })
     document.querySelector('.rightArrow').addEventListener('click', function() {
         id += 1
-        console.log(`${id} Right arrow`)
         goFetch(id)
-        console.log(`${id} Right arrow after call`)
     })
-    console.log(`${id} get MCU before goFetch`)
-    // function leftMCU() {
-    //     id -= 1
-    //     console.log(id)
-    //     goFetch()
-    //     console.log(id)
-    // }
-
-    // function rightMCU() {
-    //     id += 1
-    //     console.log(id)
-    //     goFetch()
-    //     console.log(id)
-    // }
-
-
     
     function goFetch(id) {
         fetch(`https://mcuapi.herokuapp.com/api/v1/movies/${id}`)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            console.log(`${id} in Fetch after id assignment`)
-            // console.log(data) // to see all the data
-            // // // // console.log(data.overview) // to see specific property
             document.querySelector('h2').innerText = data.title
             document.querySelector('.movieID').innerText = data.id
-            // document.body.style.backgroundImage = `url(${data.cover_url})`
             document.querySelector('img').src = data.cover_url
             document.querySelector('h3').innerText = `Directed by: ${data.directed_by}`
             document.querySelector('.overview').innerText = `${data.overview}`
@@ -61,10 +35,9 @@ function getMCU() {
         });
     }
 
-
 goFetch(id)
-console.log(`${id} after fetch call`)
 
+// to unhide arrows and trailer only once
     function unhide() {
         document.querySelector('.hidden').classList.toggle('hidden');
         document.querySelector('.hiddenAlso').classList.toggle('hiddenAlso');
@@ -74,6 +47,4 @@ console.log(`${id} after fetch call`)
 unhide()
 
 }
-
-// getMCU()
 
